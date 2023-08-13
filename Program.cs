1a82d8ad0da7a5bb76f58fab3a873fbaa3c7569a
Alex G <101558615+Alex-G-R@users.noreply.github.com>
2023-08-14 01:21:18 +0200

    Another array learning experience

--- Program.cs
+++ Program.cs
@@ -4,86 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tablice
+namespace Tab_Ww
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            // declare 5x5 2d array
-            int[,] numbers = new int [5, 5];
-
-            // declare random as r
+            int[,] tab = new int[5, 5];
             Random r = new Random();
 
-
-            // fill up the numbers with random numbers
-            for (int i = 0; i < numbers.GetLength(0); i++)
+            for(int i = 0; i < tab.GetLength(0); i++)
             {
-                for (int j = 0; j < numbers.GetLength(1); j++)
+                for(int j = 0; j < tab.GetLength(1); j++)
                 {
-                    // Random number betwen 1 and 9
-                    int rInt = r.Next(1,10);
-                    numbers[i, j] = rInt;
+                    // fill up the array tab with:
+                    // Random numbers betwen 1 and 9
+                    tab[i, j] = r.Next(1, 10);
                 }
             }
 
-            // Print the numbers
-            for (int i = 0; i < numbers.GetLength(0); i++)
+            // print the array tab to the console
+            for(int i = 0; i < tab.GetLength(0); i++)
             {
-                for (int j = 0; j < numbers.GetLength(1); j++)
+                for(int j = 0;j  < tab.GetLength(1); j++)
                 {
-                    Console.Write(numbers[i, j]);
-                    Console.Write("  ");
+                    Console.Write(tab[i, j]);
+                    Console.Write(" ");
                 }
-                Console.WriteLine("  ");
+                Console.WriteLine(" ");
             }
-            Console.WriteLine();
 
-
-            // Sum up every number in the array
-            int everyNumber = 0;
-            for(int i = 0; i < numbers.GetLength(0); i++)
+            // calculate the diagonals
+            int mainDiagonal = 0;
+            int antiDiagonal = 0;
+            for(int i = 0; i < tab.GetLength(0); i++)
             {
-                for(int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    everyNumber += numbers[i, j];
-                }
+                mainDiagonal += tab[i, i];
+                antiDiagonal += tab[i, tab.GetLength(0) - i - 1];
             }
-            Console.WriteLine($"Sum of every number in the array: {everyNumber}");
-
-
-            // Sum up first column
-            int sumFirstColumn = 0;
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                sumFirstColumn += numbers[i, 0];
-            }
-            Console.WriteLine($"Sum of every number in the first column: {sumFirstColumn}");
-
-
-            // Sum up second row
-            int sumSecondRow = 0;
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                sumSecondRow += numbers[1, i];
-            }
-            Console.WriteLine($"Sum of every number in the second row: {sumSecondRow}");
-
-
-            //Calculate diagonals
-            int diagonalSum = 0;
-            int antiDiagonalSum = 0;
-
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                diagonalSum += numbers[i, i];
-                antiDiagonalSum += numbers[i, numbers.GetLength(0) - i - 1];
-            }
-            Console.WriteLine($"Sum of main diagonal: {diagonalSum}");
-            Console.WriteLine($"Sum of anti-diagonal: {antiDiagonalSum}");
-
+            Console.WriteLine("Sum of main diagonal: " + mainDiagonal);
+            Console.WriteLine("Sum of anti diagonal: " + antiDiagonal);
 
             Console.ReadKey();
         }
